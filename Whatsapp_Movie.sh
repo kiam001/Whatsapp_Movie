b3587cc85f265cce3d2f95c9750c686f8ea6bc92
kiam001 <blablatupfen@gmail.com>
2025-05-13 12:09:16 +0200

    Update Whatsapp_Movie.sh
    fix typo

--- Whatsapp_Movie.sh
+++ Whatsapp_Movie.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i /sdcard/Downloads/in* -c copy -map 0 -segment_time 00:00:50 -f segment -reset_timestamps 1 /sdcard/Downloads/output%03d.mp4
+ffmpeg -i /sdcard/Download/in* -c copy -map 0 -segment_time 00:00:50 -f segment -reset_timestamps 1 /sdcard/Download/output%03d.mp4
